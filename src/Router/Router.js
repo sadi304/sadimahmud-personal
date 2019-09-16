f9b03f3764431f0eb71860af9576fab601cfb5ea
sadi304 <sadisagar@gmail.com>
2019-09-16 23:03:27 +0600

    Add logo and title

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -16,9 +16,16 @@ const AppRouter = () => (
   <Router>
     <div className="app">
       <div className="app-sidemenu">
+        <div>
+          <h3 className="headline">Sadi Mahmud</h3>
+        </div>
+        <div>
+          <img className="logo rounded" src="https://scontent.fdac7-1.fna.fbcdn.net/v/t1.0-9/66747232_2826164240731182_3259381205543419904_o.jpg?_nc_cat=105&_nc_oc=AQlcJ142FoZoNN90XiQdTjfc2R9twCj3HlDUUF5TYpvtYzTvJ6AcjhliG6ooWZ59Bwk&_nc_ht=scontent.fdac7-1.fna&oh=057fb920d7044bd5396f7b38967e9f7d&oe=5E38BACA" alt="profile-pic" />
+          <h6 className="text-center white">logo.png</h6>
+        </div>
         <ul className="sidemenu-parent">
           <li>
-            <ion-icon name="arrow-dropright"></ion-icon> src
+            <ion-icon name="arrow-dropdown"></ion-icon> src
             <ul className="sidemenu">
               <li>
                 <ion-icon name="contact"></ion-icon>
